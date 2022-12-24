735c44f9febb1ecbb01d1bc5bbf84b38bc4a8a53
YesYee <106434572+YesYee@users.noreply.github.com>
2022-12-24 14:04:36 +0800

    Initial Commit

---
+++ practice/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  practice
+//
+//  Created by Yee on 2022/12/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ practice/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  practice
+//
+//  Created by Yee on 2022/12/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
